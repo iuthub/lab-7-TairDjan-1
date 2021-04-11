f8a9e90bda8ddc5270ba434be90ce66b3d33d046
TairDjan <t.djanibekov@student.inha.uz>
2021-04-11 23:29:30 +0500

    almost done

---
+++ sql/imdb_queries.sql
@@ -0,0 +1,72 @@
+
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(1000) NOT NULL,
+  `body` text DEFAULT NULL,
+  `postDate` date NOT NULL DEFAULT current_timestamp(),
+  `username` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(255) NOT NULL,
+  `pwd` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usernameFK` (`username`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `usernameFK` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- webpage/index.php
+++ webpage/index.php
@@ -1,73 +1,61 @@
-<?php
-include('connection.php');
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>My Personal Page</title>
-		<link href="style.css" type="text/css" rel="stylesheet" />
-	</head>
-	
-	<body>
-		<?php include('header.php'); ?>
-		<!-- Show this part if user is not signed in yet -->
-		<div class="twocols">
-			<form action="index.php" method="post" class="twocols_col">
-				<ul class="form">
-					<li>
-						<label for="username">Username</label>
-						<input type="text" name="username" id="username" />
-					</li>
-					<li>
-						<label for="pwd">Password</label>
-						<input type="password" name="pwd" id="pwd" />
-					</li>
-					<li>
-						<label for="remember">Remember Me</label>
-						<input type="checkbox" name="remember" id="remember" checked />
-					</li>
-					<li>
-						<input type="submit" value="Submit" /> &nbsp; Not registered? <a href="register.php">Register</a>
-					</li>
-				</ul>
-			</form>
-			<div class="twocols_col">
-				<h2>About Us</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur libero nostrum consequatur dolor. Nesciunt eos dolorem enim accusantium libero impedit ipsa perspiciatis vel dolore reiciendis ratione quam, non sequi sit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nobis vero ullam quae. Repellendus dolores quis tenetur enim distinctio, optio vero, cupiditate commodi eligendi similique laboriosam maxime corporis quasi labore!</p>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>My Simple Blog</title>
+	<link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+	<div class="container">
+		<!-- login nav -->
+		<div class="nav">
+				<div class="navitem">
+					<input type="text" name="username" form="loginForm" placeholder="Username" />
+				</div>
+				<div class="navitem">
+					<input type="password" name="pwd" form="loginForm" placeholder="Password" />
+				</div>
+				<div class="navitem">
+					<form id="loginForm" action="index.php?action=login" method="post">
+						<input type="submit" value="Login" />
+					</form>
+				</div>
+				<div class="navitem">
+					<a href="register.php" class="btn">Register</a>
+				</div>
+		</div>
+		<!-- logout nav -->
+		<div class="nav">
+			
+				<div class="navitem">
+					<a href="edit.php" class="btn">New Post</a>
+				</div>
+				<div class="navitem">
+					<form action="index.php?action=logout" method="post">
+						<input type="submit" value="Logout" />
+					</form>
+				</div>
+			
+		</div>
+		<div class="main">
+			<div class="article">
+				<div class="article-header">
+					<h2>Title</h2>
+				</div>
+				<div class="article-body">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi beatae praesentium quia temporibus aliquid facere expedita sint, molestias hic illo harum aspernatur sequi repudiandae doloremque tenetur, veritatis animi doloribus!
+				</div>
+				<div class="article-footer">
+					<div class="article-meta">
+						Edit: date
+					</div>
+					<div class="article-actions">
+						<a href="edit.php?id=">Edit</a> | 
+						<a href="delete.php?id=">Delete</a>
+					</div>
+				</div>
 			</div>
 		</div>
-		
-		<!-- Show this part after user signed in successfully -->
-		<div class="logout_panel"><a href="register.php">My Profile</a>&nbsp;|&nbsp;<a href="index.php?logout=1">Log Out</a></div>
-		<h2>New Post</h2>
-		<form action="index.php" method="post">
-			<ul class="form">
-				<li>
-					<label for="title">Title</label>
-					<input type="text" name="title" id="title" />
-				</li>
-				<li>
-					<label for="body">Body</label>
-					<textarea name="body" id="body" cols="30" rows="10"></textarea>
-				</li>
-				<li>
-					<input type="submit" value="Post" />
-				</li>
-			</ul>
-		</form>
-		<div class="onecol">
-			<div class="card">
-				<h2>TITLE HEADING</h2>
-				<h5>Author, Sep 2, 2017</h5>
-				<p>Some text..</p>
-				<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-			</div>
-			<div class="card">
-				<h2>TITLE HEADING</h2>
-				<h5>Author, Sep 2, 2017</h5>
-				<p>Some text..</p>
-				<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-			</div>
-		</div>
-	</body>
+	</div>
+</body>
 </html>
